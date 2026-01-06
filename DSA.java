966c4e81b315d1220356b0016349ba39c13851c0
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-06 23:29:53 +0530

    implemented array sorting checking using recursion

--- DSA.java
+++ DSA.java
@@ -211,3 +211,18 @@
 //        findOccurence(str, 0, element);
 // }
 
+// To check the array is sorted (strictly increasing)
+public static boolean isSorted(int idx , int arr[]){
+    if(idx == arr.length-1){
+        return true;
+    }
+    if(arr[idx]<arr[idx+1]){
+         return isSorted(idx+1, arr);
+    }else{
+        return false;
+    }
+}
+public static void main(String args[]){
+    int arr[] = {1,2,3};
+    System.out.println(isSorted(0, arr));
+}
