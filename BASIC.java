2e3f088ec3526101f654e434953a75fecd808e70
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-10 22:49:00 +0530

    commented codes

--- BASIC.java
+++ BASIC.java
@@ -139,28 +139,28 @@
 // }
 
 // //program for for loop, while loop, do-while loop
- public class BASIC {
-    public static void main(String[] args) {
-        // For Loop
-        System.out.println("For Loop:");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Iteration: " + i);
-        }
+//  public class BASIC {
+//     public static void main(String[] args) {
+//         // For Loop
+//         System.out.println("For Loop:");
+//         for (int i = 1; i <= 5; i++) {
+//             System.out.println("Iteration: " + i);
+//         }
 
-        // While Loop
-        System.out.println("\nWhile Loop:");
-        int j = 1;
-        while (j <= 5) {
-            System.out.println("Iteration: " + j);
-            j++;
-        }
+//         // While Loop
+//         System.out.println("\nWhile Loop:");
+//         int j = 1;
+//         while (j <= 5) {
+//             System.out.println("Iteration: " + j);
+//             j++;
+//         }
 
-        // Do-While Loop
-        System.out.println("\nDo-While Loop:");
-        int k = 1;
-        do {
-            System.out.println("Iteration: " + k);
-            k++;
-        } while (k <= 5);
-    }
-}
+//         // Do-While Loop
+//         System.out.println("\nDo-While Loop:");
+//         int k = 1;
+//         do {
+//             System.out.println("Iteration: " + k);
+//             k++;
+//         } while (k <= 5);
+//     }
+// }
