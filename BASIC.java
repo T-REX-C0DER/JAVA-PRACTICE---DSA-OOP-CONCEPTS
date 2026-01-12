5b8a32b54410faa7d5782a971fdc75cd37c972a5
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-12 22:32:23 +0530

    deleted code

--- BASIC.java
+++ BASIC.java
@@ -169,25 +169,3 @@
 // abstract class Animal {
 //     abstract void sound();
 // }
-
-// class Dog extends Animal {
-//     void sound() {
-//         System.out.println("The dog barks.");
-//     }
-// }
-
-// class Cat extends Animal {
-//     void sound() {
-//         System.out.println("The cat meows.");
-//     }
-// }
-
-// public class BASIC {
-//     public static void main(String[] args) {
-//         Animal myDog = new Dog();
-//         Animal myCat = new Cat();
-
-//         myDog.sound();
-//         myCat.sound();
-//     }
-// }
