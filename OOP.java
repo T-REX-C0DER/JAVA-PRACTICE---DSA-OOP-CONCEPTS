158014129044c98d1347878846e7efefaa0d5ad4
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-12 22:46:30 +0530

    Encapsulation code

--- OOP.java
+++ OOP.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 // //wap to show the use of this keyword
 
 // class Person {
@@ -93,7 +94,7 @@
 //     }
 // }
 
-// // program to show abstraction anf getters , setters in oops
+// // program to show abstraction in oops
 // abstract class bike{
 //     abstract void run();
 // }
@@ -108,3 +109,55 @@
 //         b.run();
 //     }
 // }
+
+// program to show encapsulation in oops
+class Student{
+    private String name;
+    private int age;
+
+    //getter method for name
+    public String getName(){
+        return name;
+    }
+
+    //setter method for name
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //getter method for age
+    public int getAge(){
+        return age;
+    }
+
+    //setter method for age
+    public void setAge(int age){
+        if(age>0){
+            this.age = age;
+        }else{
+            System.out.println("Invalid age");
+        }
+    }
+}
+public class OOP{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sanjay");
+        s.setAge(21);
+
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
+
+// program to show use of arraylist in java
+// import java.util.ArrayList;                                                                  
+public class OOP{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);   
+        System.out.println("ArrayList: " + list);
+    }
+}
