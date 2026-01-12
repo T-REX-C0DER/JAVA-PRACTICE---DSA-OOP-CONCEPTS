838aaa8b1e019e678f65fa93853b8ae701c4d159
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-12 22:24:02 +0530

    Commented code

--- DSA.java
+++ DSA.java
@@ -276,21 +276,21 @@
 // }
 
 
-// To print all permutations of a string
-public class DSA {
-      public static void printperm(String str , String perm){
-            if(str.length() == 0){
-                  System.out.println(perm);
-                  return;
-            }
-            for(int i=0;i<str.length();i++){
-                  char currchar = str.charAt(i);
-                  String newstr = str.substring(0, i) + str.substring(i+1);
-                  printperm(newstr, perm + currchar);
-            }
-      }
-      public static void main(String args[]){
-            String str = "abc";
-            printperm(str, "");
-      }
-      }
+// // To print all permutations of a string
+// public class DSA {
+//       public static void printperm(String str , String perm){
+//             if(str.length() == 0){
+//                   System.out.println(perm);
+//                   return;
+//             }
+//             for(int i=0;i<str.length();i++){
+//                   char currchar = str.charAt(i);
+//                   String newstr = str.substring(0, i) + str.substring(i+1);
+//                   printperm(newstr, perm + currchar);
+//             }
+//       }
+//       public static void main(String args[]){
+//             String str = "abc";
+//             printperm(str, "");
+//       }
+//       }
