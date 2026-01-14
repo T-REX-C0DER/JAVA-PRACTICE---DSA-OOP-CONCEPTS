26aa5b790d471f8cb17ae6ef54a83d64b493103f
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-14 17:00:09 +0530

    QUICKSORT & Commented code

--- DSA.java
+++ DSA.java
@@ -319,59 +319,97 @@
 //         }
 // }
 
-// MERGESORT ALGORITHM
-import java.util.*;
-public class DSA {
-       public static void merge(int arr[] , int si , int mid , int ei){
-              int temp[] = new int[ei - si + 1];
-              int i = si; //iterator for left part
-              int j = mid + 1; //iterator for right part
-              int k = 0; //iterator for temp array
+// // MERGESORT ALGORITHM
+// import java.util.*;
+// public class DSA {
+//        public static void merge(int arr[] , int si , int mid , int ei){
+//               int temp[] = new int[ei - si + 1];
+//               int i = si; //iterator for left part
+//               int j = mid + 1; //iterator for right part
+//               int k = 0; //iterator for temp array
 
-              while(i <= mid && j <= ei){
-                     if(arr[i] <= arr[j]){
-                            temp[k] = arr[i];
-                            i++;
-                     }else{
-                            temp[k] = arr[j];
-                            j++;
-                     }
-                     k++;
-              }
+//               while(i <= mid && j <= ei){
+//                      if(arr[i] <= arr[j]){
+//                             temp[k] = arr[i];
+//                             i++;
+//                      }else{
+//                             temp[k] = arr[j];
+//                             j++;
+//                      }
+//                      k++;
+//               }
 
-              //left part
-              while(i <= mid){
-                     temp[k] = arr[i];
-                     i++;
-                     k++;
-              }
+//               //left part
+//               while(i <= mid){
+//                      temp[k] = arr[i];
+//                      i++;
+//                      k++;
+//               }
 
-              //right part
-              while(j <= ei){
-                     temp[k] = arr[j];
-                     j++;
-                     k++;
-              }
+//               //right part
+//               while(j <= ei){
+//                      temp[k] = arr[j];
+//                      j++;
+//                      k++;
+//               }
 
-              //copying temp to original array
-              for(k = 0 , i = si ; k < temp.length ; k++ , i++){
-                     arr[i] = temp[k];
-              }
-       }
+//               //copying temp to original array
+//               for(k = 0 , i = si ; k < temp.length ; k++ , i++){
+//                      arr[i] = temp[k];
+//               }
+//        }
 
-       public static void mergeSort(int arr[] , int si , int ei){
-              if(si >= ei){
-                     return;
-              }
-              int mid = si + (ei - si) / 2;
-              mergeSort(arr, si, mid);
-              mergeSort(arr, mid + 1, ei);
-              merge(arr, si, mid, ei);
-       }
+//        public static void mergeSort(int arr[] , int si , int ei){
+//               if(si >= ei){
+//                      return;
+//               }
+//               int mid = si + (ei - si) / 2;
+//               mergeSort(arr, si, mid);
+//               mergeSort(arr, mid + 1, ei);
+//               merge(arr, si, mid, ei);
+//        }
 
-       public static void main(String args[]){
-              int arr[] = {38, 27, 43, 3, 9, 82, 10};
-              mergeSort(arr, 0, arr.length - 1);
-              System.out.println(Arrays.toString(arr));
-       }
-}
+//        public static void main(String args[]){
+//               int arr[] = {38, 27, 43, 3, 9, 82, 10};
+//               mergeSort(arr, 0, arr.length - 1);
+//               System.out.println(Arrays.toString(arr));
+//        }
+// }
+// // QUICK SORT ALGORITHM
+// // import java.util.*;
+// public class DSA {
+//          public static int partition(int arr[] , int si , int ei){
+//                   int pivot = arr[ei];
+//                   int i = si - 1;
+    
+//                   for(int j = si ; j < ei ; j++){
+//                         if(arr[j] <= pivot){
+//                              i++;
+//                              //swap arr[i] and arr[j]
+//                              int temp = arr[i];
+//                              arr[i] = arr[j];
+//                              arr[j] = temp;
+//                         }
+//                   }
+//                   //swap arr[i+1] and arr[ei] (or pivot)
+//                   int temp = arr[i + 1];
+//                   arr[i + 1] = arr[ei];
+//                   arr[ei] = temp;
+    
+//                   return i + 1;
+//          }
+    
+//          public static void quickSort(int arr[] , int si , int ei){
+//                   if(si < ei){
+//                         int pi = partition(arr, si, ei);
+//                         quickSort(arr, si, pi - 1);
+//                         quickSort(arr, pi + 1, ei);
+//                   }
+//          }
+    
+//          public static void main(String args[]){
+//                   int arr[] = {10, 7, 8, 9, 1, 5};
+//                   quickSort(arr, 0, arr.length - 1);
+//                   System.out.println(Arrays.toString(arr));
+//          }
+//     }
