4eefb05ebc86fc6364d3cdaa290cc11574ddaf54
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-14 16:58:54 +0530

    Merge Sort

--- DSA.java
+++ DSA.java
@@ -318,3 +318,60 @@
 //                 printperm(str, "", freq);
 //         }
 // }
+
+// MERGESORT ALGORITHM
+import java.util.*;
+public class DSA {
+       public static void merge(int arr[] , int si , int mid , int ei){
+              int temp[] = new int[ei - si + 1];
+              int i = si; //iterator for left part
+              int j = mid + 1; //iterator for right part
+              int k = 0; //iterator for temp array
+
+              while(i <= mid && j <= ei){
+                     if(arr[i] <= arr[j]){
+                            temp[k] = arr[i];
+                            i++;
+                     }else{
+                            temp[k] = arr[j];
+                            j++;
+                     }
+                     k++;
+              }
+
+              //left part
+              while(i <= mid){
+                     temp[k] = arr[i];
+                     i++;
+                     k++;
+              }
+
+              //right part
+              while(j <= ei){
+                     temp[k] = arr[j];
+                     j++;
+                     k++;
+              }
+
+              //copying temp to original array
+              for(k = 0 , i = si ; k < temp.length ; k++ , i++){
+                     arr[i] = temp[k];
+              }
+       }
+
+       public static void mergeSort(int arr[] , int si , int ei){
+              if(si >= ei){
+                     return;
+              }
+              int mid = si + (ei - si) / 2;
+              mergeSort(arr, si, mid);
+              mergeSort(arr, mid + 1, ei);
+              merge(arr, si, mid, ei);
+       }
+
+       public static void main(String args[]){
+              int arr[] = {38, 27, 43, 3, 9, 82, 10};
+              mergeSort(arr, 0, arr.length - 1);
+              System.out.println(Arrays.toString(arr));
+       }
+}
