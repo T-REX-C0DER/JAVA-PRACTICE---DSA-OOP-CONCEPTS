fb0e700c11e6f7701ecc7508e64d6c8c24c78af6
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-19 20:49:44 +0530

    Commented Codes

--- DSA.java
+++ DSA.java
@@ -517,36 +517,36 @@
 //     }
 // }
 
-// STACK USING Collections FRAMEWORK
-import java.util.*;
+// // STACK USING Collections FRAMEWORK
+// import java.util.*;
 
-public class DSA {
-    public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
+// public class DSA {
+//     public static void main(String[] args) {
+//         Stack<Integer> stack = new Stack<>();
 
-        // Push elements onto the stack
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+//         // Push elements onto the stack
+//         stack.push(10);
+//         stack.push(20);
+//         stack.push(30);
 
-        System.out.println("Stack: " + stack);
+//         System.out.println("Stack: " + stack);
 
-        // Pop an element from the stack
-        int poppedElement = stack.pop();
-        System.out.println("Popped Element: " + poppedElement);
-        System.out.println("Stack after pop: " + stack);
+//         // Pop an element from the stack
+//         int poppedElement = stack.pop();
+//         System.out.println("Popped Element: " + poppedElement);
+//         System.out.println("Stack after pop: " + stack);
 
-        // Peek at the top element of the stack
-        int topElement = stack.peek();
-        System.out.println("Top Element: " + topElement);
-        System.out.println("Stack after peek: " + stack);
+//         // Peek at the top element of the stack
+//         int topElement = stack.peek();
+//         System.out.println("Top Element: " + topElement);
+//         System.out.println("Stack after peek: " + stack);
 
-        // Check if the stack is empty
-        boolean isEmpty = stack.isEmpty();
-        System.out.println("Is Stack Empty? " + isEmpty);
+//         // Check if the stack is empty
+//         boolean isEmpty = stack.isEmpty();
+//         System.out.println("Is Stack Empty? " + isEmpty);
 
-        // Get the size of the stack
-        int size = stack.size();
-        System.out.println("Stack Size: " + size);
-    }
-}
+//         // Get the size of the stack
+//         int size = stack.size();
+//         System.out.println("Stack Size: " + size);
+//     }
+// }
