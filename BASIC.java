8d727238328fdd60c1a78ba7dafb9e4408b3b841
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-22 23:23:08 +0530

    Duplicate array

--- BASIC.java
+++ BASIC.java
@@ -212,3 +212,20 @@
 //         System.out.println("Difference: " + difference);
 //     }
 // }
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; // points to last unique element
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; // number of unique elements
+    }
+}
