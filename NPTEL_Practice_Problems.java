4ddb5148d247dab244cbf5b55d0e869b86a677b9
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-26 18:21:47 +0530

    Assignment 1 PP4

--- NPTEL_Practice_Problems.java
+++ NPTEL_Practice_Problems.java
@@ -1,60 +1,63 @@
 import java.util.*;
 
+// Assignment 1
+
 // public class NPTEL_Practice_Problems {
 //     public static void main(String[] args) {
 //         Scanner in = new Scanner(System.in);
 //         int number = in.nextInt();
-
 //         if(number % 2 == 0 ){
 //             System.out.println("Even");
 //         }else{
 //             System.out.println("Odd");
 //         }
-
 //         in.close();
 //     }
 // }
-
 
 
 // public class NPTEL_Practice_Problems {
 //     public static void main(String[] strings) {
 //           double width;
 //           double height;
-
 //           Scanner in = new Scanner(System.in);
 //           width = in.nextDouble();
 //           height = in.nextDouble();
-
 //           // calculate perimeter 
 //           double perimeter  = 2*(width+height);
-
 //           // calculate area of rectangle
 //           double area = height*width;
- 
 //           System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f\n", height,width,perimeter);
-//           System.out.printf("Area is %.1f * %1.f  = %.2f", height,width,area);
-        
+//           System.out.printf("Area is %.1f * %1.f  = %.2f", height,width,area);    
 //           in.close();
 //     }
 // }
 
 
+// public class NPTEL_Practice_Problems {
+//     public static void main(String[] strings) {
+//           double radius;
+//           double height;
+//           double Pie = 3.14;
+//           Scanner in = new Scanner(System.in);
+//           radius = in.nextDouble();
+//           height = in.nextDouble();
+//           double volume = Pie*radius*radius*height;
+//           System.out.printf("Volume is : %.2f",volume);
+//           in.close();
+//     }
+// }
 
-public class NPTEL_Practice_Problems {
-    public static void main(String[] strings) {
-          double radius;
-          double height;
-          double Pie = 3.14;
 
-          Scanner in = new Scanner(System.in);
-          radius = in.nextDouble();
-          height = in.nextDouble();
+// public class NPTEL_Practice_Problems {
+//     public static void main(String[] strings) {
+//           Scanner in = new Scanner(System.in);
+//           int number = in.nextInt();
 
-          double volume = Pie*radius*radius*height;
+//           for(int i = 1 ; i < 6 ; i++){
+//             System.out.printf("%d * %d = %d \n" , number , i , number*i);
+//           }
 
-          System.out.printf("Volume is : %.2f",volume);
-
-          in.close();
-    }
-}
+//           in.close();
+//     }
+// }
