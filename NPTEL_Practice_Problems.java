a8e58ee18f32894a0046fb68d5257ccce2062f76
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-26 18:12:34 +0530

    Assignment 1 PP3

--- NPTEL_Practice_Problems.java
+++ NPTEL_Practice_Problems.java
@@ -17,24 +17,44 @@ import java.util.*;
 
 
 
+// public class NPTEL_Practice_Problems {
+//     public static void main(String[] strings) {
+//           double width;
+//           double height;
+
+//           Scanner in = new Scanner(System.in);
+//           width = in.nextDouble();
+//           height = in.nextDouble();
+
+//           // calculate perimeter 
+//           double perimeter  = 2*(width+height);
+
+//           // calculate area of rectangle
+//           double area = height*width;
+ 
+//           System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f\n", height,width,perimeter);
+//           System.out.printf("Area is %.1f * %1.f  = %.2f", height,width,area);
+        
+//           in.close();
+//     }
+// }
+
+
+
 public class NPTEL_Practice_Problems {
     public static void main(String[] strings) {
-          double width;
+          double radius;
           double height;
+          double Pie = 3.14;
 
           Scanner in = new Scanner(System.in);
-          width = in.nextDouble();
+          radius = in.nextDouble();
           height = in.nextDouble();
 
-          // calculate perimeter 
-          double perimeter  = 2*(width+height);
+          double volume = Pie*radius*radius*height;
 
-          // calculate area of rectangle
-          double area = height*width;
- 
-          System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f\n", height,width,perimeter);
-          System.out.printf("Area is %.1f * %1.f  = %.2f", height,width,area);
-        
+          System.out.printf("Volume is : %.2f",volume);
+
           in.close();
     }
 }
