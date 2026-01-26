e0647514738892145dae21a57e17dd8a2b6e5514
T-Rex_Coder <sanjaylade92220@gmail.com>
2026-01-26 18:07:42 +0530

    Assignment 1 PP2

--- NPTEL_Practice_Problems.java
+++ NPTEL_Practice_Problems.java
@@ -1,16 +1,40 @@
 import java.util.*;
 
+// public class NPTEL_Practice_Problems {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         int number = in.nextInt();
+
+//         if(number % 2 == 0 ){
+//             System.out.println("Even");
+//         }else{
+//             System.out.println("Odd");
+//         }
+
+//         in.close();
+//     }
+// }
+
+
+
 public class NPTEL_Practice_Problems {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+    public static void main(String[] strings) {
+          double width;
+          double height;
 
-        if(number % 2 == 0 ){
-            System.out.println("Even");
-        }else{
-            System.out.println("Odd");
-        }
+          Scanner in = new Scanner(System.in);
+          width = in.nextDouble();
+          height = in.nextDouble();
 
-        in.close();
+          // calculate perimeter 
+          double perimeter  = 2*(width+height);
+
+          // calculate area of rectangle
+          double area = height*width;
+ 
+          System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f\n", height,width,perimeter);
+          System.out.printf("Area is %.1f * %1.f  = %.2f", height,width,area);
+        
+          in.close();
     }
 }
